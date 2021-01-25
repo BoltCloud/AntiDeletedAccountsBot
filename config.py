3bc2a2111c1074477bf596e711444a651ddef4bc
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-01-25 06:08:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = 1408232012:AAHYT2G3BK6zBgmTa0KfQt8XRzcLRNn9tSA
-SESSION_NAME = antideletedaccountsbot
-ID = 1721131
-HASH = f6972fb44d1c4773fac957344fbeaf63
-SUPERADMIN = 1314948019
+token = 1408232012:AAHYT2G3BK6zBgmTa0KfQt8XRzcLRNn9tSA
+session_name = antideletedaccountsbot
+id = 1721131
+hash = f6972fb44d1c4773fac957344fbeaf63
+superadmin = 1314948019
